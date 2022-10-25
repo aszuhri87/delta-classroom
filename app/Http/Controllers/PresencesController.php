71ac622664cf5217ebe7e9e9bf09817a755aad4c
Achmad Saefuddin Zuhri <achmad.s.zuhri182@gmail.com>
2022-10-25 15:16:06 +0700

    presence

--- app/Http/Controllers/PresencesController.php
+++ app/Http/Controllers/PresencesController.php
@@ -57,9 +57,9 @@ class PresencesController extends Controller
             return Redirect::back()->withErrors(['message' => 'Presensi gagal!, Anda presensi di kelas ini'])->withInput();
         }
 
-        if ($class->created_at < date('Y-m-d H:i:s')) {
-            return Redirect::back()->withErrors(['message' => 'Presensi gagal!, Kode kadaluarsa'])->withInput();
-        }
+        // if ($class->created_at < date('Y-m-d H:i:s', '1')) {
+        //     return Redirect::back()->withErrors(['message' => 'Presensi gagal!, Kode kadaluarsa'])->withInput();
+        // }
 
         if ($request->presence_code == $class->presence_code) {
             Presence::create([
