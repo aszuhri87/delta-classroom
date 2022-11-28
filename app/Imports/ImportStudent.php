de2ed696af83319ab6d3df1422e0983b6c07f19f
Achmad Saefuddin Zuhri <achmad.s.zuhri182@gmail.com>
2022-11-28 15:24:29 +0700

    fix import

--- app/Imports/ImportStudent.php
+++ app/Imports/ImportStudent.php
@@ -4,10 +4,13 @@ namespace App\Imports;
 
 use App\Models\Student;
 use Illuminate\Support\Facades\Hash;
+use Maatwebsite\Excel\Concerns\SkipsEmptyRows;
+use Maatwebsite\Excel\Concerns\SkipsOnError;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithValidation;
 use PhpOffice\PhpSpreadsheet\Shared\Date;
 
-class ImportStudent implements ToModel
+class ImportStudent implements ToModel, WithValidation, SkipsEmptyRows, SkipsOnError
 {
     /**
      * @return \Illuminate\Database\Eloquent\Model|null
@@ -30,5 +33,19 @@ class ImportStudent implements ToModel
             'school_origin' => $row[5],
             'password' => Hash::make($pass),
         ]);
+    }
+
+    public function rules(): array
+    {
+        return [
+            '1' => 'required',
+        ];
+    }
+
+    public function onError(\Throwable $e)
+    {
+        $error = "Import failed, check your Excel file first! make sure the row is not empty or have duplicate data on unique field.";
+
+        return \Redirect::back()->withErrors(['message' => $error])->withInput();
     }
 }
